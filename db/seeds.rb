701efd4f145ba3e71010c1f45b65e7e784abd7c7
Patrick O'Brien <obrien.patrickl@gmail.com>
2018-04-10 17:00:17 -0700

    small ui fix, fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ Status.destroy_all
 OperatingSystem.destroy_all
 Datacenter.destroy_all
 Role.destroy_all
+SoftwareApp.destroy_all
 
 statuses = Status.create!([
   { name: 'setup' },
@@ -33,29 +34,37 @@ operating_systems = OperatingSystem.create!([
 
 roles = Role.create!([
   { name: 'Bitter' },
+  { name: 'Dataserver' },
+  { name: 'BitterAndDataserver' },
   { name: 'dns-slave' },
   { name: 'FactServ' }
 ])
 
+software_apps = SoftwareApp.create!([
+  { name: 'Bitter', roles: [Role.first, Role.third] },
+  { name: 'Dataserver', roles: [Role.second, Role.third] }
+])
+
+
 nodes = Node.create!([
   {
-    name: 'tor-ns1', fqdn: 'tor-ns1.tor.example.org', status_id: 1, datacenter_id: 1, roles: ['2'],
+    name: 'tor-ns1', fqdn: 'tor-ns1.tor.example.org', status_id: 1, datacenter_id: 1, roles: [Role.fourth],
     operating_system_id: 1, internal_ip_address: '10.10.10.10', management_ip_address: '10.10.10.10'
   },
   {
-    name: 'tor-ns2', fqdn: 'tor-ns2.tor.example.org', status_id: 1, datacenter_id: 1, roles: ['2'],
+    name: 'tor-ns2', fqdn: 'tor-ns2.tor.example.org', status_id: 1, datacenter_id: 1, roles: [Role.fourth],
     operating_system_id: 1, internal_ip_address: '10.10.10.11', management_ip_address: '10.10.10.11'
   },
   {
-    name: 'tor-bit001', fqdn: 'tor-bit001.ops.example.org', status_id: 4, datacenter_id: 2, roles: ['1'],
+    name: 'tor-bit001', fqdn: 'tor-bit001.ops.example.org', status_id: 4, datacenter_id: 2, roles: [Role.first],
     operating_system_id: 3, internal_ip_address: '10.10.10.12', management_ip_address: '10.10.10.12'
   },
   {
-    name: 'tor-bit002', fqdn: 'tor-bit002.ops.example.org', status_id: 4, datacenter_id: 2, roles: ['1'],
+    name: 'tor-bit002', fqdn: 'tor-bit002.ops.example.org', status_id: 4, datacenter_id: 2, roles: [Role.first],
     operating_system_id: 3, internal_ip_address: '10.10.10.13', management_ip_address: '10.10.10.13'
   },
   {
-    name: 'va1-combo001', fqdn: 'va1-combo001.ops.example.org', status_id: 4, datacenter_id: 1, roles: ['1', '2'],
+    name: 'va1-combo001', fqdn: 'va1-combo001.ops.example.org', status_id: 4, datacenter_id: 1, roles: [Role.third],
     operating_system_id: 3, internal_ip_address: '10.10.10.14', management_ip_address: '10.10.10.14'
   },
 ])
